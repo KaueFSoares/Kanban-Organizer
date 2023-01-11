11e42c7c85d9ef265f3158e8cf175f91e454462d
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-01-10 23:22:37 -0300

    Home page started

--- files/kanban-organizer/src/components/pages/home/Home.js
+++ files/kanban-organizer/src/components/pages/home/Home.js
@@ -1,11 +1,48 @@
+import { useState } from "react"
+
 import "./home.sass"
 
 function Home() {
+
+  const [selected, setSelected] = useState("login")
+
+  function changeToLogin() {
+    setSelected("login")
+    console.log(selected)
+  }
+
+  function changeToSignup(){
+    setSelected("signup")
+    console.log(selected)
+  }
+
+
+
   return (
-    <main id="container">
-      <h1>Main Content</h1>
-      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.</p>
-      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.</p>
+    <main id="home-container">
+      <div id="content-box">
+
+      </div>
+
+      <div id="sign-box">
+        <header>
+          <button
+            className="button"
+            id="login-button"
+            onClick={changeToLogin}
+          >
+            <p>Login</p>
+          </button>
+
+          <button
+            className="button"
+            id="signup-button"
+            onClick={changeToSignup}
+          >
+            <p>Sign up</p>
+          </button>
+        </header>
+      </div>
     </main>
   )
 }
