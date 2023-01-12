5d676dd7f20706d94b2c3377e9030a4e5bdeebf1
Kauê Soares <114025415+KaueFSoares@users.noreply.github.com>
2023-01-12 19:30:46 -0300

    Login form turned into a component

--- files/kanban-organizer/src/components/pages/home/SignForm/SignForm.js
+++ files/kanban-organizer/src/components/pages/home/SignForm/SignForm.js
@@ -1,29 +1,19 @@
 import { useEffect, useState } from "react"
-import { AiOutlineMail, AiOutlineLock, AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai"
-import { FaFacebookF, FaTwitter, FaGoogle } from "react-icons/fa"
+import LoginForm from "./LoginForm/LoginForm"
+
 
 import "./signform.sass"
 
 function SignForm() {
 
     const [selected, setSelected] = useState("login")
-    const [email, setEmail] = useState("")
-    const [inputType, setInputType] = useState("password")
-    const [password, setPassword] = useState("")
+
 
     useEffect(() => {
         console.log(selected)
-    }, [selected]) 
+    }, [selected])
 
-    console.log(email, password)
 
-    function changeInputType(){
-        if (inputType === "login"){
-            setInputType("password")
-        } else {
-            setInputType("login")
-        }
-    }
 
     return (
         <div id="sign-box">
@@ -51,82 +41,7 @@ function SignForm() {
 
 
             {(selected === "login") ? (
-                <div id="login-box">
-                    {/* --- LOGIN BOX --- */}
-
-                    {/* email */}
-                    <div id="email-box">
-                        <p>
-                            <AiOutlineMail />
-                        </p>
-
-                        <input
-                            type="email"
-                            placeholder="E-mail"
-                            onChange={(e) => {
-                                setEmail(e.target.value)
-                            }}
-                        />
-                    </div>
-
-                    {/* password */}
-                    <div id="password-box">
-                        <p id = "lock-icon">
-                            <AiOutlineLock />
-                        </p>
-
-                        <input
-                            type={`${inputType}`}
-                            placeholder="Password"
-                            onChange={(e) => {
-                                setPassword(e.target.value)
-                            }}
-                        />
-
-                        <button id = "eye-icon"onClick = {changeInputType}>
-                            {(inputType === "password" ? (
-                                <AiOutlineEye />
-                            ) : (
-                                <AiOutlineEyeInvisible />
-                            ))}
-                        </button>
-                    </div>
-
-                    {/* remember me / forgot password? */}
-                    <div id = "remember-me-box">
-                        <div id = "remember-me">
-                            <input type="checkbox" />
-                            <p>Remember me</p>
-                        </div>
-
-                        <div>
-                            <a href = "/" id = "forgot-password">
-                                Forgot password?
-                            </a>
-                        </div>
-                    </div>
-
-                    {/* login button */}
-                    <div id = "login-button-box">
-                        <button id = "login-button">
-                                <p>Login</p>
-                        </button>
-                    </div>
-
-                    {/* other login options */}
-                    <div id = "other-login-options-box">
-                        <p id = "label">
-                            Or login using 
-                        </p>
-
-                        <div id = "other-options">
-                            <button id = "facebook"><FaFacebookF /></button>
-                            <button id = "tt"><FaTwitter /></button>
-                            <button id = "google"><FaGoogle /></button>
-                        </div>
-                    </div>
-
-                </div>
+                <LoginForm />
             ) : (
                 <div>
                     {/* --- SIGN UP BOX --- */}
